4b49251cf023559815e37e84b4d4831f2d66fb4b
gabyvespasiano <gabyvespasiano@gmail.com>
2023-04-08 20:22:09 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const navLinks = document.querySelectorAll('nav ul li a');
+const navLinks = document.querySelectorAll('ul li a');
 
 // Agrega un listener de clic a cada enlace
 navLinks.forEach(link => {
@@ -21,3 +21,31 @@ navLinks.forEach(link => {
     }
   }});
 });
+
+let imagenes=[];
+fetch('/assets/trabajos/proyectos.json')
+  .then(response => response.json())
+  .then(trabajos => {
+    const trabajosHTML = trabajos.map(trabajo =>
+     `
+    <article class="trabajos-tarjeta">
+      <main class="trabajos-tarjeta-contenedor">
+        ${imagen(trabajo.imagen)}
+        <h2>${trabajo.nombre}</h2>
+        <p>${trabajo.descripcion}</p>
+        <a href="${trabajo.link}" target="_blank"><button>Visitar</button></a>                    
+      </main>
+    </article>
+    `).join('');
+    
+    document.querySelector('.contenedor-trabajos').innerHTML = trabajosHTML;
+    // console.log(document.querySelectorAll(".trabajos-tarjeta").length);
+    // console.log(imagenes);
+    for (var i = 0; i < document.querySelectorAll(".trabajos-tarjeta").length ; i++) {
+       document.querySelector(".trabajos-tarjeta:nth-child("+(i+1)+")").style.backgroundImage = "url('"+ imagenes[i] +"')";
+    }
+  });
+  function imagen(a){
+    imagenes.push(a)
+    return ""
+  }
